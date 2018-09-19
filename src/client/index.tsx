552fb6262d1bb5e24c48fd4360c8f9cb2c33e3d7
Fabio Salimbeni <fabio.salimbeni@gmail.com>
2018-09-19 16:46:16 +0100

    Remove src folder

--- src/client/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
-
-const App: React.StatelessComponent<{}> = () => (
-	<div>Hello World from the client</div>
-)
-
-ReactDOM.render(<App />, document.querySelector("#root"))
