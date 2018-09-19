917eb50d94a023c08ae1411a6472b21643b6618a
Fabio Salimbeni <fabio.salimbeni@gmail.com>
2018-09-19 16:41:07 +0100

    REmove testing folder

--- test/acceptance/tests/test.js
+++
@@ -1,8 +0,0 @@
-describe('test', () => {
-  it('should load the browser', browser => {
-    browser
-      .url('http://localhost:4000')
-      .expect.element('div#root')
-      .text.to.contain('Hello World')
-  })
-})
